c4e98fbb08a8c7d677b60b92c4fc48c304b13fb9
stewie <stewie@syneforge.com>
2022-04-16 16:26:07 +0300

    finish songs table,add formatters

--- components/SongsTable.tsx
+++ components/SongsTable.tsx
@@ -7,9 +7,13 @@ import {
     Th,
     Tbody,
     IconButton,
+    Image,
+    Flex,
+    Text,
 } from '@chakra-ui/react';
 import { BsFillPlayFill } from 'react-icons/bs';
 import { AiOutlineClockCircle } from 'react-icons/ai';
+import { formatDate, formatTime } from '../lib/formatters';
 
 const SongsTable = ({ songs }) => {
     return (
@@ -39,6 +43,38 @@ const SongsTable = ({ songs }) => {
                         </Th>
                     </Tr>
                 </Thead>
+                <Tbody>
+                    {songs.map((song, i) => (
+                        <Tr
+                            sx={{
+                                transition: 'all .3s',
+                                '&:hover': { bg: 'rbga(255,255,255,0.1)' },
+                            }}
+                            key={song.id}
+                            cursor="cursor"
+                        >
+                            <Td>{i + 1}</Td>
+                            <Td>
+                                <Flex>
+                                    <Box marginX="20px">
+                                        <Image
+                                            src={`https://picsum.photos/200?random=${i}`}
+                                            width="40px"
+                                        />
+                                    </Box>
+                                    <Flex direction="column">
+                                        <Text> {song.name}</Text>
+                                        <Text color="gray.200">
+                                            {song.artist.name}
+                                        </Text>
+                                    </Flex>
+                                </Flex>
+                            </Td>
+                            <Td>{formatDate(song.createdAt)}</Td>
+                            <Td>{formatTime(song.duration)}</Td>
+                        </Tr>
+                    ))}
+                </Tbody>
             </Table>
         </Box>
     );
